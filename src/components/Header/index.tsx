06d020ea6d2753b013cfec3180d0267d4e181a5a
danieltwentynine <danielmcardoso2016@protonmail.com>
2025-07-27 13:57:13 -0400

    adicionando header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,30 @@
+import { HeaderBar, Links, LinkItems, LinkCart } from './styles'
+import logo from '../../assets/images/logo.svg'
+import carrinho from '../../assets/images/carrinho.svg'
+
+const Header = () => (
+  <HeaderBar>
+    <div>
+      <img src={logo} alt="EPlay" />
+      <nav>
+        <Links>
+          <LinkItems>
+            <a href="#">Categorias</a>
+          </LinkItems>
+          <LinkItems>
+            <a href="#">Novidades</a>
+          </LinkItems>
+          <LinkItems>
+            <a href="#">Promoções</a>
+          </LinkItems>
+        </Links>
+      </nav>
+    </div>
+    <LinkCart href="#">
+      0 - produto(s)
+      <img src={carrinho} alt="Carrinho" />
+    </LinkCart>
+  </HeaderBar>
+)
+
+export default Header
